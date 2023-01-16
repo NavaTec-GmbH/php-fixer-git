b0e33ab06ed685ba866c330739365062fc4ea81e
NAVATEC\pfeffermannma <markus.pfeffermann@navatec.de>
2023-01-16 20:27:35 +0100

    Fix bin autoload.

--- src/index.php
+++ src/index.php
@@ -1,7 +1,5 @@
 <?php
 
-require __DIR__.'/../vendor/autoload.php';
-
 use Stringy\StaticStringy as S;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
